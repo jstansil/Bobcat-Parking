cfcdbac23fc007c82ac3e68191ea538ec57fb756
jstansil <jdstansil@gmail.com>
2017-11-16 15:35:57 -0800

    second screen added. northern lots are on screen main3activity (i fucked up on naming) and southern lots are on the default screen (main_activity)

--- BobcatParkingApp/app/src/main/java/com/example/amandaabalos/bobcatparkingapp/Main3Activity.java
+++ BobcatParkingApp/app/src/main/java/com/example/amandaabalos/bobcatparkingapp/Main3Activity.java
@@ -1,16 +1,16 @@
 package com.example.amandaabalos.bobcatparkingapp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//this is a second version of MainActivity that represents the 2nd screen containing northern parking lots
+public class Main3Activity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-   /*myDB is the instance*/
+    /*myDB is the instance*/
     //Database myDB;
     public ParkingLot [] lots;
     private LotUpdater update;
@@ -19,35 +19,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main3);
         //myDB = new Database(this);
         update = new LotUpdater();
 
-        //case where we move from the 2nd screen. we don't need to remake the parking array, we just recieve it from the 2nd screen
-        if (getIntent().hasExtra("com.example.amandaabalos.bobcatparkingapp.southlots")) {
-            lots = (ParkingLot[])getIntent().getExtras().getSerializable("com.example.amandaabalos.bobcatparkingapp.southlots");
+        //initialize all the parking lots. could probably find a prettier way to do this
+        //also, the values put in for distance & max capacity are wrong
+        //they are arranged by their relative northern position on campus. permit values were taken from http://taps.ucmerced.edu/parkingmaps
+
+        if (getIntent().hasExtra("com.example.amandaabalos.bobcatparkingapp.northlots")) {
+            lots = (ParkingLot[])getIntent().getExtras().getSerializable("com.example.amandaabalos.bobcatparkingapp.northlots");
             //2nd screen is passed the appropriate parking lot object. need to access and display its info
-        }
-        else {
-            //initialize all the parking lots. could probably find a prettier way to do this
-            //also, the values put in for distance & max capacity are wrong
-            //they are arranged by their relative northern position on campus. permit values were taken from
-            // http://taps.ucmerced.edu/parkingmaps
-            lots = new ParkingLot[9];
-            lots[0] = new ParkingLot(200, "X;A;ACP;AUB;ALEV;B;BCP;DP;MP;AM;M;C;FSLEV;HCP;CCP", 10.0, "Lake One");
-            lots[1] = new ParkingLot(300, "X;DP;MP;ALEV;A;ACP;AUB;FSLEV;B;BCP;CCP;C;HCP", 12.0, "Lake Two");
 
-            lots[2] = new ParkingLot(15, "X;DP;MP;ALEV;A;ACP;AUB;FSLEV;B;BCP;C;CCP;HCP", 8.5, "ECEC");
-
-            lots[3] = new ParkingLot(400, "X;DP;MP;ALEV;A;ACP;AUB;FSLEV;B;BCP;CCP;C;HCP", 11.0, "Evolution");
-
-            lots[4] = new ParkingLot(20, "X;ACP;AUB;DP;EV;AM;VRIDE;EZPARK", 5.5, "Library One");
-            lots[5] = new ParkingLot(15, "A;ACP;ALEV;MP", 5.0, "Library Two");
-
-            lots[6] = new ParkingLot(100, "X;A;ACP;AUB;EZPARK;DP;MP;AM;ALEV", 2.5, "Le Grand");
-
-            lots[7] = new ParkingLot(200, "X;A;ACP;AUB;ALEV;LEV;B;BCP;DP;MP", 5.5, "North Bowl One");
-            lots[8] = new ParkingLot(250, "X;A;ACP;AUB;ALEV;LEV;B;BCP;DP;MP;FSC", 6.0, "North Bowl Two");
         }
         //Initial update of parking lots. this will fetch their status from the DB before they are displayed
         for(ParkingLot l: lots){
@@ -60,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
         Button lot2 = (Button)findViewById(R.id.button2);
         Button lot3 = (Button)findViewById(R.id.button3);
         Button lot4 = (Button)findViewById(R.id.button4);
-        Button northlots = (Button)findViewById(R.id.moveup);
+        Button lot5 = (Button)findViewById(R.id.button5);
+        Button southlots = (Button)findViewById(R.id.movedown);
+
 
         //Each button press sends a different lot object to the 2nd screen
         lot1.setOnClickListener(new View.OnClickListener() {
@@ -95,14 +80,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        northlots.setOnClickListener(new View.OnClickListener() {
+        lot5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), Main3Activity.class);
+                Intent i = new Intent(getApplicationContext(), ParkingInfo.class);
+                i.putExtra("com.example.amandaabalos.bobcatparkingapp.lotrequest", lots[4]);
+                startActivity(i);
+            }
+        });
+        southlots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 i.putExtra("com.example.amandaabalos.bobcatparkingapp.northlots", lots);
                 startActivity(i);
             }
         });
+
 
         //Activity that calls the lot updater every minute (based on user clock)
         //Later we can also augment this to send/recieve info from the database server every minute
